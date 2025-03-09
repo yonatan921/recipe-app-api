bd6074fa2b8306eb40e102bd4f3ce0145a1efc33
yonataba <yonataba@post.bgu.acl.il>
2025-03-09 19:28:44 +0200

    Finish user

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,3 +15,12 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user

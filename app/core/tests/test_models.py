7f5605f3ab9fbc93417b9417df7298a7000261c1
yonataba <yonataba@post.bgu.acl.il>
2025-03-11 20:56:44 +0200

    Create recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,8 @@ Test for models
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+from core import models
 
 
 class ModelTests(TestCase):
@@ -55,3 +57,17 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            "test123",
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='sample recipe description.'
+        )
+        self.assertEqual(str(recipe), recipe.title)
